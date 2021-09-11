f2450887c26ca2ffbc387448412eda6b4ac7a12b
dankrainbow <79637386+dankrainbow@users.noreply.github.com>
2021-09-11 15:29:59 +0100

    Update main.py
    added main content

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+import os
+import time
+import colorama
+from colorama import Fore, Back, init
+
+Percent = 0
+
+init(convert=True)
+for i in range(100):
+    Percent += 1
+    print(Fore.BLUE+ "Loading Epic Things....." + str(Percent) + "Percent has been completed")
+    time.sleep(0.25) # edit the number for the delay between each second
+os.system("start https://www.youtube.com/watch?v=xvFZjo5PgG0")
+os.system("start msg.vbs")
+time.sleep(10)
