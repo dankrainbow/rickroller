4e933c5918b8da1dfb3703f42bfc5b7b2bc5a5be
dankrainbow <79637386+dankrainbow@users.noreply.github.com>
2021-11-17 18:32:04 +0000

    Update main.py
    add sleep. uncomment to enable

--- main.py
+++ main.py
@@ -12,5 +12,9 @@ for i in range(100):
     print(Fore.BLUE+ "Loading Epic Things....." + str(Percent) + "Percent has been completed")
     time.sleep(0.25) # edit the number for the delay between each second
 os.system("start https://www.youtube.com/watch?v=xvFZjo5PgG0")
+##time.sleep(4)
+## added sleep because the msg.vbs launched before the rickroll loaded
+## change number to how many seconds delay between load rickroll and vbs opens
+## uncomment to enable
 os.system("start msg.vbs")
 time.sleep(10)
